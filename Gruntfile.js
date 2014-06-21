c54adab7f9f3b9311324ef279f3c87c0cd7c771c
Joel Carr <joel.ben.carr@gmail.com>
2014-06-21 12:50:13 +0100

    ignored files not needed for _site/

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,40 @@
+'use strict';
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
         // read in the project settings from package.json into pkg propert. This allows us to refer
         // to the values of properties within our package.json
-        pkg: grunt.file.readJSON("package.json"),
+        pkg: grunt.file.readJSON('package.json'),
 
         // the configuration object for a task lives as a property on the configuration object,
-        // that"s named the same as the task - so "watch" task goes in our config object under the "watch" key
+        // that's named the same as the task - so 'watch' task goes in our config object under the 'watch' key
 
         // watch task detects changes to files and runs specified tasks
         watch: {
-            css: {
-                files: "assets/scss/**/*.scss",
-                tasks: ["sass"],
-                options: {
-                    livereload: true
-                }
+            // livereload: {
+            //     options: {
+            //         livereload: true
+            //     }
+            // },
+            scss: {
+                files: 'assets/scss/**/*.scss',
+                tasks: ['sass'],
+                // options: {
+                //     livereload: false
+                // }
             },
             images: {
-                files: "assets/imgs_src/**/*.{png,jpg,gif}",
-                tasks: ["imagemin"]
+                files: 'assets/imgs_src/**/*.{png,jpg,gif}',
+                tasks: ['imagemin']
             },
             scripts: {
-                files: "assets/js/*.js",
-                tasks: ["jshint"],
+                files: 'assets/js/**/*.js',
+                tasks: ['jshint'],
             },
-            livereload: {
-                options: {
-                    livereload: true
-                },
-                files: [
-                    "assets/css/*.css",
-                    "assets/js/*/**.js"
-                ]
-            }
+            // css: {
+            //     files: 'assets/css/main.css'
+            // }
         },
 
         sass: {
@@ -49,10 +50,10 @@ module.exports = function(grunt) {
 
         jshint: {
             options: {
-                jshintrc: ".jshintrc"
+                jshintrc: '.jshintrc'
             },
             files: {
-                src: ["Gruntfile.js", "assets/js/**/*.js", "!assets/js/libs/*.js"]
+                src: ['Gruntfile.js', 'assets/js/**/*.js', '!assets/js/libs/*.js', '!assets/js/require.js', '!assets/js/require-min.js']
 
             }
         },
@@ -69,13 +70,13 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-sass");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask("default", [
-        "watch"
+    grunt.registerTask('default', [
+        'watch'
     ]);
 
 };
