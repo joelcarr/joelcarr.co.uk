8aa588bed863151ed03bc1857fddf184d9c35c9d
Joel Carr <joelcarr@Joels-Mac-mini.local>
2025-12-31 16:05:24 +0000

    chore: indent code using spaces

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,14 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
-	eleventyConfig.addPassthroughCopy("src/assets/site.css");
-	eleventyConfig.setBrowserSyncConfig({
-		files: "assets/",
-	});
+  eleventyConfig.addPassthroughCopy("src/assets/site.css");
+  eleventyConfig.setBrowserSyncConfig({
+    files: "assets/",
+  });
 
-	return {
-		dir: {
-			input: "src",
-			output: "_site",
-		},
-	};
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
 };
